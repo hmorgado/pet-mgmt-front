d4190c4088bbdcdcc24f67006c61cd4e2e126c99
Heitor Morgado <heitor.morgado@justeattakeaway.com>
2023-11-26 12:23:09 -0600

    delete works

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 
-function Pet({name}){
-  return(
-    <div>
-      <h1>{name}</h1>
-    </div>
-  )
-}
 
 function App() {
   const [data, setData] = useState(null);
   useEffect(() => {
     fetch(
-      `http://localh.ost:8080/rest/pets`
+      `http://localhost:8080/rest/pets`
     )
-    .then((response) => {
-      console.log(response)
-    })
-  });
+    .then((response) => response.json())
+    .then(setData);
+  }, []);
 
- 
-  return(
-    <div> heitor </div>
-  );
+  const handleDelete = (id) => () => {
+    fetch(`http://localhost:8080/rest/pets/${id}`, { method: 'DELETE', mode: 'cors' })
+
+  }
+
+
+  if (data) {
+    return(
+      <div className="Pet">
+      {data.map((pet) => (
+        <h1 key={pet.id}>{pet.id} {pet.name} 
+          <button onClick={handleDelete(pet.id)}>
+          delete
+          </button>
+        </h1>
+      ))}
+    </div>
+    )
+  }
+  return <h1>Kitties</h1>;
 }
 
 export default App;
