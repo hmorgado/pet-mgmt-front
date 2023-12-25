2667941ee0fcf8a3c695ccbd6bd86a0418fbb647
Heitor Morgado <heitor.morgado@justeattakeaway.com>
2023-12-24 20:18:16 -0700

    main radio form pet type

--- src/MyForm.js
+++ src/MyForm.js
@@ -1,38 +1,58 @@
 import React from 'react';
 import { Formik, Field, Form } from 'formik';
 
-function MyForm(){
+function MyForm() {
 
-    async function addKitty(values){
+    async function addPet(values) {
         fetch(
-            `http://localhost:8080/rest/pets`,{
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({
-                    "name": values.catName,
-                    "petType": {
-                        "id": 2,
-                    }
-                })
-            }
+            `http://localhost:8080/rest/pets`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                "name": values.petName,
+                "petType": {
+                    "id": values.petTypeId,
+                }
+            })
+        }
         )
+            .then((response) => response.json())
+            .then((response) => {
+                console.log(response)
+            })
     }
 
-    return(
+    return (
         <div>
-            <h1>Add kitty</h1>
+            <h1>Add Pet</h1>
             <Formik
                 initialValues={{
-                    catName: '',
+                    petName: '',
                     type: 2,
                 }}
-            onSubmit={(values) => addKitty(values)}
+                onSubmit={(values) => addPet(values)}
             >
-            <Form>
-                <label htmlFor="catName">Cat Name   </label>
-                <Field id="catName" name="catName"></Field>
-                <button type="submit">Submit</button>
-            </Form>
+                <Form>
+                    <label htmlFor="petName">Pet Name   </label>
+                    <Field id="petName" name="petName"></Field>
+                    <br />
+                    <label htmlFor="petName">
+                        Pet Type
+                        <label>
+                            <Field type="radio" name="petTypeId" value="1" />
+                            Bird
+                        </label>
+                        <label>
+                            <Field type="radio" name="petTypeId" value="2" />
+                            Cat
+                        </label>
+                        <label>
+                            <Field type="radio" name="petTypeId" value="3" />
+                            Dog
+                        </label>
+                    </label>  <br />
+                    <button type="submit">Submit</button>
+                </Form>
             </Formik>
         </div>
     )
